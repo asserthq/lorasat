72bf27ae0088b0496c8165462922f23998fd0f81
asserthq <sanya2003ogorodov@gmail.com>
2026-09-04 15:43:33 +0700

    add to sat protocols & beacon

--- core/src/message/beacon.rs
+++ core/src/message/beacon.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, defmt::Format)]
 pub struct Beacon {
     pub sat_addr: u32,
     pub interval_sec: u16,
--- core/src/message/client_data.rs
+++ core/src/message/client_data.rs
@@ -3,7 +3,7 @@ use serde::{Deserialize, Serialize};
 
 pub const MAX_CLIENT_DATA: usize = 255;
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, defmt::Format)]
 pub struct ClientData {
     pub data: Vec<u8, MAX_CLIENT_DATA>,
 }
